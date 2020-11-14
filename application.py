98fd35f7a2223d76ba1bdbc02bb4ebc4c6bf6627
Ammon Taylor <ammont@Ammons-MBP.lan>
2020-11-13 21:22:26 -0700

    added a CORS fix

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from urllib.parse import parse_qs
 from lyrics import getLyrics
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def main():
